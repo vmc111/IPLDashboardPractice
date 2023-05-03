a21f9851057460a72b70f3cd6c6fef38cd36a3f2
vmc111 <vamship625@gmail.com>
2023-05-03 08:00:34 +0000

    two components in second page

--- src/components/LatestMatch/index.js
+++ src/components/LatestMatch/index.js
@@ -1,1 +1,37 @@
-// Write your code here
+import './index.css'
+
+const LatestMatch = props => {
+  const latestMatch = props
+  const details = latestMatch.latestMatch
+  console.log(details)
+
+  return (
+    <div className="main-card">
+      <div className="main-card-2">
+        <div className="details">
+          <h4>{details.competingTeam}</h4>
+          <h5>{details.date}</h5>
+          <h6>{details.venue}</h6>
+          <h6>{details.result}</h6>
+        </div>
+        <img
+          src={details.competingTeamLogo}
+          alt={details.id}
+          className="oponent-logo"
+        />
+      </div>
+      <div className="match-stats">
+        <h4>First Innings</h4>
+        <h6>{details.firstInnings}</h6>
+        <h4>Second Innings</h4>
+        <h6>{details.secondInnings}</h6>
+        <h4>Man of The Match</h4>
+        <h6>{details.manOfTheMatch}</h6>
+        <h4>Umpires</h4>
+        <h6>{details.umpires}</h6>
+      </div>
+    </div>
+  )
+}
+
+export default LatestMatch
--- src/components/TeamMatches/index.js
+++ src/components/TeamMatches/index.js
@@ -1,20 +1,113 @@
 import {Component} from 'react'
+import Loader from 'react-loader-spinner'
 import './index.css'
+import LatestMatch from '../LatestMatch'
 
 export default class Matches extends Component {
-  state = {matchesObject: {}, fetchingDetails: true}
+  state = {matchesObject: {}, fetchingDetails: true, backgroundColor: 'white'}
 
   componentDidMount() {
-    this.getteamMatches()
+    this.getTeamMatches()
   }
 
-  getteamMatches = async () => {
-    const response = await fetch('https://apis.ccbp.in/ipl/:id')
+  getTeamMatches = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+    const response = await fetch(`https://apis.ccbp.in/ipl/${id}`)
     const data = await response.json()
-    console.log(data)
+    const formattedData = {
+      teamBannerUrl: data.team_banner_url,
+      latestMatchDetails: {
+        competingTeam: data.latest_match_details.competing_team,
+        competingTeamLogo: data.latest_match_details.competing_team_logo,
+        date: data.latest_match_details.date,
+        firstInnings: data.latest_match_details.first_innings,
+        id: data.latest_match_details.id,
+        manOfTheMatch: data.latest_match_details.man_of_the_match,
+        matchStatus: data.latest_match_details.match_status,
+        result: data.latest_match_details.result,
+        secondInnings: data.latest_match_details.second_innings,
+        umpires: data.latest_match_details.umpires,
+        venue: data.latest_match_details.venue,
+      },
+
+      recentMatches: data.recent_matches.map(eachObject => ({
+        competingTeam: eachObject.competing_team,
+        competingTeamLogo: eachObject.competing_team_logo,
+        date: eachObject.date,
+        firstInnings: eachObject.first_innings,
+        id: eachObject.id,
+        manOfTheMatch: eachObject.man_of_the_match,
+        matchStatus: eachObject.match_status,
+        result: eachObject.result,
+        secondInnings: eachObject.second_innings,
+        umpires: eachObject.umpires,
+        venue: eachObject.venue,
+      })),
+    }
+
+    let background
+
+    switch (id) {
+      case 'SH':
+        background = 'red-background'
+        break
+      case 'DC':
+        background = 'navyBlue-red'
+        break
+      case 'RCB':
+        background = 'red-gold'
+        break
+      case 'KKR':
+        background = 'purple-gold'
+        break
+      case 'MI':
+        background = 'blue-orange'
+        break
+      case 'CSK':
+        background = 'blue-yellow'
+        break
+      case 'RR':
+        background = 'blue-gold'
+        break
+      case 'KXP':
+        background = 'red-lightGray'
+        break
+
+      default:
+        background = 'white'
+    }
+
+    this.setState({
+      matchesObject: formattedData,
+      fetchingDetails: false,
+      backgroundColor: background,
+    })
   }
 
   render() {
-    return <h1>Hii Friends</h1>
+    const {matchesObject, fetchingDetails, backgroundColor} = this.state
+    const loader = (
+      <div>
+        <Loader type="Oval" color="#ffffff" height={50} width={50} />
+      </div>
+    )
+    const body = (
+      <>
+        <img
+          src={matchesObject.teamBannerUrl}
+          alt="team banner"
+          className="banner-img"
+        />
+        <h3>Latest Match</h3>
+        <LatestMatch latestMatch={matchesObject.latestMatchDetails} />
+      </>
+    )
+    return (
+      <div className={`bg-matches ${backgroundColor}`}>
+        {fetchingDetails ? loader : body}
+      </div>
+    )
   }
 }
